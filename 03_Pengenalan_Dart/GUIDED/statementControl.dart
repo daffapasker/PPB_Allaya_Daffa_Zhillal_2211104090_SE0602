6ab7f1b2293272a6b75cad90b69842c835aa1cd0
daffapasker <162289697+daffapasker@users.noreply.github.com>
2024-10-02 23:28:05 +0700

    Update statementControl.dart

--- 03_Pengenalan_Dart/GUIDED/statementControl.dart
+++ 03_Pengenalan_Dart/GUIDED/statementControl.dart
@@ -1,1 +1,31 @@
+void main() {
+   var openHours = 8; 
+  var closedHours = 21; 
+  var now = 17; 
+  
+  if (now > openHours && now < closedHours) { 
+    print("Oooyy, wis bukak kie ndan !"); 
+  } else { 
+    print("Ngapurane yak, anu wis tutup je.."); 
+  } 
+}
 
+
+void main() { 
+ var day = 'b'; // Misalkan 1 = Senin, 2 = Selasa, dst. 
+  
+  switch (day) { 
+    case 'a': 
+      print("nilai sangat bagus"); 
+      break; 
+    case 'b': 
+      print("nilai bagus"); 
+      break; 
+    case 'c': 
+      print("nilai cukup"); 
+      break; 
+    default: 
+      print("nilai tidak tersedia"); 
+  } 
+
+}
