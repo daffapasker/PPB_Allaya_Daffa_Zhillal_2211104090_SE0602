6e38ef4eacf9d08736dbe6b20ba256411eaab876
daffapasker <162289697+daffapasker@users.noreply.github.com>
2024-10-02 23:27:07 +0700

    Update loop.dart

--- 03_Pengenalan_Dart/GUIDED/loop.dart
+++ 03_Pengenalan_Dart/GUIDED/loop.dart
@@ -1,1 +1,16 @@
+void main() { 
+// For loop untuk mencetak angka 1 sampai 10 
+for (int i = 1; i <= 10; i++) { 
+print(i); 
+} 
+} 
 
+
+void main() { 
+int i = 1; // Inisialisasi variabel 
+// While loop untuk mencetak angka 1 sampai 10 
+while (i <= 10) { 
+print('Angka: $i'); 
+i++; // Increment untuk menghindari loop tak berujung 
+} 
+}
