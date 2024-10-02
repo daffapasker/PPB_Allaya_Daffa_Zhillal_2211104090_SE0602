f3029e9cb91c59b46dd6253a45f05600de7b4c99
daffapasker <162289697+daffapasker@users.noreply.github.com>
2024-10-02 23:30:22 +0700

    Update fungsi.dart

--- 03_Pengenalan_Dart/GUIDED/fungsi.dart
+++ 03_Pengenalan_Dart/GUIDED/fungsi.dart
@@ -1,1 +1,16 @@
+void cetakPesan(String pesan) { 
+print(pesan); 
+}
 
+int perkalian (int a,int b) {
+  return a * b;
+}
+
+void main() { 
+
+  int hasil = perkalian (4, 5); 
+  print('Hasil kali: $hasil');  
+
+  cetakPesan('Halo, Dart!'); 
+
+}
