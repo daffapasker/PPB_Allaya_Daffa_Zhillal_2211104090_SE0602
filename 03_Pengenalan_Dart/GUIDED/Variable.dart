e624b8efc4dc11d81fb168cd657a61498c19f24e
daffapasker <162289697+daffapasker@users.noreply.github.com>
2024-10-02 23:23:02 +0700

    Update Variable.dart

--- 03_Pengenalan_Dart/GUIDED/Variable.dart
+++ 03_Pengenalan_Dart/GUIDED/Variable.dart
@@ -1,1 +1,21 @@
+void main() { 
+// Menggunakan var 
+var name = "Allaya Daffa Zhillal"; // Tipe data String 
+var age = 20; // Tipe data Integer 
+print("Nama: $name, Usia: $age"); 
+}
 
+void main() { 
+// Type annotation 
+String name = "DAFFA"; // Tipe data String 
+int age = 20; // Tipe data Integer 
+print("Nama: $name, Usia: $age"); 
+}
+
+void main() { 
+// Multiple variable 
+String firstName, lastName; // Tipe data String 
+firstName = "Allaya Daffa"; 
+lastName = "Zhillal"; 
+print("Nama Lengkap: $firstName $lastName"); 
+}
