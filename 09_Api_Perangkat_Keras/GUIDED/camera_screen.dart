73619f6f8a6539cf57df57273f4548d7772ded03
daffapasker <162289697+daffapasker@users.noreply.github.com>
2024-12-23 01:19:07 +0700

    Create camera_screen.dart

---
+++ 09_Api_Perangkat_Keras/GUIDED/camera_screen.dart
@@ -0,0 +1,81 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'package:praktikum_9/display_screen.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+
+    _controller = CameraController(
+      firstCamera,
+      ResolutionPreset.high,
+    );
+    _initializeControllerFuture = _controller.initialize();
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Camera Implementation'),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent[600],
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            // Capture the image as bytes
+            final XFile image = await _controller.takePicture();
+            final Uint8List imageBytes = await image.readAsBytes();
+
+            // Navigate to DisplayScreen with the image bytes
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayScreen(imageBytes: imageBytes),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
