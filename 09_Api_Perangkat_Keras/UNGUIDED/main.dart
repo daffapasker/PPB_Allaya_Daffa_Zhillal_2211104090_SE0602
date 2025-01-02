4f9610de4735ae27551565e4a59508242f1f2cc7
daffapasker <162289697+daffapasker@users.noreply.github.com>
2025-01-02 13:24:49 +0700

    Update main.dart

--- 09_Api_Perangkat_Keras/UNGUIDED/main.dart
+++ 09_Api_Perangkat_Keras/UNGUIDED/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_9/camera_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Guided_09',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: CameraScreen(), // Set the home screen to CameraScreen
+    );
+  }
+}
